28abb6ede41ab45d47d8823aef883c465e388434
Maxime <machanwei@gmail.com>
2026-02-23 07:05:26 +0000

    feat: enhance deploy script to prompt for CUSTOM_USER and PASSWORD if not set

--- environments/local-k8s/namespaces/obsidian/deploy.sh
+++ environments/local-k8s/namespaces/obsidian/deploy.sh
@@ -19,6 +19,18 @@ if [[ -z "${OBSIDIAN_HOST}" ]]; then
   echo "OBSIDIAN_HOST=${OBSIDIAN_HOST}" >> .env
 fi
 
+if [[ -z "${CUSTOM_USER}" ]]; then
+  read -p "Enter the custom user: " CUSTOM_USER
+  export CUSTOM_USER=${CUSTOM_USER}
+  echo "CUSTOM_USER=${CUSTOM_USER}" >> .env
+fi
+
+if [[ -z "${PASSWORD}" ]]; then
+  read -p "Enter the password: " PASSWORD
+  export PASSWORD=${PASSWORD}
+  echo "PASSWORD=${PASSWORD}" >> .env
+fi
+
 # replace the variables in the obsidian.install.yaml
 envsubst < obsidian/obsidian.install.yaml | kubectl -n ${NAMESPACE} apply -f -
 
